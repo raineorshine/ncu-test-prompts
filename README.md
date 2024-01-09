424ac1b2a7c76e2fb557183a0f2ea92df00940aa
Raine Revere <raine@cybersemics.org>
2024-01-09 14:47:56 +0000

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+A package that tests terkelg/prompts.
+
+Used for testing [npm-check-updates](https://github.com/raineorshine/npm-check-updates).
---
+++ index.js
@@ -0,0 +1,18 @@
+const prompts = require('prompts');
+
+(async () => {
+  const response = await prompts([
+    {
+      type: 'multiselect',
+      name: 'color',
+      message: 'Pick colors',
+      choices: [
+        { title: 'Red', value: '#ff0000' },
+        { title: 'Green', value: '#00ff00' },
+        { title: 'Blue', value: '#0000ff' }
+      ],
+    }
+  ]);
+
+  console.log(response);
+})();
